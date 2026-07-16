f8f4cc553bdba0095041a82e3f8e009c1b508c9c
Aswin K S <ksaswin808@gmail.com>
2026-07-16 13:22:30 +0530

    oracle is the backend now

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function App(){
   const [message, setMessage] = useState("Loading...");
   useEffect(() => {
-    fetch("http://51.20.130.178:8000/api/hello/")
+    fetch("http://140.245.223.191:8000/api/hello/")
     .then(response => response.json())
     .then(data => setMessage(data.message))
     .catch(error => console.error(error));
